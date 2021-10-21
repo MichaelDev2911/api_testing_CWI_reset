0ea753f9aa42951453b01e25b136b209dc3047f7
MichaelDev2911 <michaelsilveira.dev@outlook.com>
2021-10-21 12:26:38 -0300

    schemaBooking.json

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
@@ -3,9 +3,14 @@ package br.com.restassuredapitesting.tests.booking.tests;
 import br.com.restassuredapitesting.base.BaseTest;
 import br.com.restassuredapitesting.suites.AllTests;
 import br.com.restassuredapitesting.tests.booking.requests.GetBookingRequest;
+import br.com.restassuredapitesting.utils.Utils;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
+import java.io.File;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 
 public class GetBookingTest extends BaseTest {
 
@@ -18,5 +23,16 @@ public class GetBookingTest extends BaseTest {
                 .then()
                 .statusCode(200)
                 .body("size()", Matchers.greaterThan(0));
+    }
+
+    @Test
+    @Category({AllTests.class})
+    public void validaSchemaDaListagemDeReservas() {
+        GetBookingRequest getBookingRequest = new GetBookingRequest();
+
+        getBookingRequest.bookingReturnIds()
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchema(new File(Utils.getSchemaBasePath("booking","bookings"))));
     }
 }
